aaf74946683ae593ced50d28fc6e4c657f4dc5a8
AgusBaudot <101721143+AgusBaudot@users.noreply.github.com>
2023-06-08 08:59:32 -0300

    Terminado el drag and drop

--- Assets/Scripts/DradAndDrop.cs
+++ Assets/Scripts/DradAndDrop.cs
@@ -7,18 +7,35 @@ using UnityEngine.UI;
 public class DradAndDrop : MonoBehaviour
 {
     public bool isClicked;
+    private Camera cam;
+    private Vector3 dragOffset;
+    private GameObject panel;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+        panel = GameObject.FindGameObjectWithTag("Panel");
+    }
+
+    private void OnMouseDown()
+    {
+        dragOffset = transform.position - GetMousePos();
+    }
+
+    private void OnMouseUp()
+    {
+        
+    }
 
     private void OnMouseDrag()
     {
-        transform.position = GetMousePos();
+        transform.position = GetMousePos() + dragOffset;
     }
 
     Vector3 GetMousePos()
     {
-        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0;
         return mousePos;
     }
-
-    //video: https://www.google.com/search?q=how+to+make+a+drag+and+drop+in+unity+2d&rlz=1C1GCEU_enAR828AR828&oq=how+to+make+a+drag+and+drop+in+unity+2d&aqs=chrome..69i57j33i22i29i30l2.100799j0j9&sourceid=chrome&ie=UTF-8#fpstate=ive&vld=cid:7a5114e6,vid:Tv82HIvKcZQ
 }
